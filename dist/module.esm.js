d589e8deb25ad961a8ca30af248f5172bf037b23
gengyudong02 <gengyudong02@58.com>
2022-06-15 19:06:31 +0800

    feat: init

---
+++ dist/module.esm.js
@@ -0,0 +1,7 @@
+function $a8e101027d325e52$export$e16d8520af44a096(a, b) {
+    return a + b + 3;
+}
+
+
+export {$a8e101027d325e52$export$e16d8520af44a096 as add};
+//# sourceMappingURL=module.esm.js.map
